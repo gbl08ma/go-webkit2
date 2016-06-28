e6b7998e74c179708007302b154ce21d195764ba
gbl08ma <gbl08ma@gmail.com>
2016-06-28 11:47:02 +0100

    Adjusted GAsyncReadyCallback to be compliant with new CGo requirements
    Via https://github.com/ividence/go-webkit2/commit/74ed6024bfab4420ff80fb62cead4e346d8692c9

--- webkit2/gasyncreadycallback.go
+++ webkit2/gasyncreadycallback.go
@@ -1,5 +1,6 @@
 package webkit2
 
+// #include <stdlib.h>
 // #include <gio/gio.h>
 // #include "gasyncreadycallback.go.h"
 import "C"
@@ -7,17 +8,38 @@ import (
 	"errors"
 	"reflect"
 	"unsafe"
+	"sync"
 )
 
 type garCallback struct {
 	f reflect.Value
 }
 
+var (
+	// CGo no longer allows pointers with pointers to Go structures to be passed to C.
+	// Instead, we use an arbitrary pointer and a lookup table.
+	// The arbitrary pointer is allocated to ensure uniqueness, but not actually used.
+	callbackMap map[C.gpointer]*garCallback
+	// Locking to ensure thread-safety.
+	callbackMapMutex sync.RWMutex
+)
+
+func init() {
+	callbackMap = make(map[C.gpointer]*garCallback)
+}
+
 //export _go_gasyncreadycallback_call
 func _go_gasyncreadycallback_call(cbinfoRaw C.gpointer, cresult unsafe.Pointer) {
 	result := (*C.GAsyncResult)(cresult)
-	cbinfo := (*garCallback)(unsafe.Pointer(cbinfoRaw))
-	cbinfo.f.Call([]reflect.Value{reflect.ValueOf(result)})
+	callbackMapMutex.Lock()
+	cbinfo, exists := callbackMap[cbinfoRaw]
+	delete(callbackMap, cbinfoRaw)
+	callbackMapMutex.Unlock()
+	// If a value existed in our lookup table, free the allocated pointer, and call the required callback.
+	if exists {
+		C.free(unsafe.Pointer(cbinfoRaw))
+		cbinfo.f.Call([]reflect.Value{reflect.ValueOf(result)})
+	}
 }
 
 func newGAsyncReadyCallback(f interface{}) (cCallback C.GAsyncReadyCallback, userData C.gpointer, err error) {
@@ -26,5 +48,10 @@ func newGAsyncReadyCallback(f interface{}) (cCallback C.GAsyncReadyCallback, use
 		return nil, nil, errors.New("f is not a function")
 	}
 	cbinfo := &garCallback{rf}
-	return C.GAsyncReadyCallback(C._gasyncreadycallback_call), C.gpointer(unsafe.Pointer(cbinfo)), nil
+	// Allocate some memory to assure a unique pointer
+	ptr := C.gpointer(C.malloc(4))
+	callbackMapMutex.Lock()
+	callbackMap[ptr] = cbinfo
+	callbackMapMutex.Unlock()
+	return C.GAsyncReadyCallback(C._gasyncreadycallback_call), ptr, nil
 }
